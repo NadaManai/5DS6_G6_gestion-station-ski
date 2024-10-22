389020ee460abec9bcceb694d76830a3cf91e981
Nour <nourelhouda.kchaou@esprit.tn>
2024-10-22 16:16:38 +0100

    nour first commit

--- src/main/java/tn/esprit/spring/repositories/ISkierRepository.java
+++ src/main/java/tn/esprit/spring/repositories/ISkierRepository.java
@@ -1,5 +1,6 @@
 package tn.esprit.spring.repositories;
 
+import org.mockito.Mock;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.data.jpa.repository.Query;
@@ -8,6 +9,7 @@ import tn.esprit.spring.entities.Subscription;
 import tn.esprit.spring.entities.TypeSubscription;
 
 import java.util.List;
+
 
 public interface ISkierRepository extends JpaRepository<Skier, Long> {
    List<Skier> findBySubscription_TypeSub(TypeSubscription typeSubscription);
