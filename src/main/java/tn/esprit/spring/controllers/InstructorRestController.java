36a4b5717d416f4cbf111096bc5b0eab63825e91
Nada Manai <nada.manai@esprit.tn>
2024-10-31 11:57:27 +0100

    Add logging for instructor module

--- src/main/java/tn/esprit/spring/controllers/InstructorRestController.java
+++ src/main/java/tn/esprit/spring/controllers/InstructorRestController.java
@@ -3,6 +3,8 @@ package tn.esprit.spring.controllers;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.spring.entities.Instructor;
 import tn.esprit.spring.services.IInstructorServices;
@@ -15,34 +17,60 @@ import java.util.List;
 @RequiredArgsConstructor
 public class InstructorRestController {
 
+    private static final Logger logger = LogManager.getLogger(InstructorRestController.class);
+    
     private final IInstructorServices instructorServices;
 
     @Operation(description = "Add Instructor")
     @PostMapping("/add")
-    public Instructor addInstructor(@RequestBody Instructor instructor){
-        return  instructorServices.addInstructor(instructor);
+    public Instructor addInstructor(@RequestBody Instructor instructor) {
+        logger.info("Received request to add instructor: {}", instructor);
+        Instructor addedInstructor = instructorServices.addInstructor(instructor);
+        logger.info("Instructor added successfully with ID: {}", addedInstructor.getNumInstructor());
+        return addedInstructor;
     }
+
     @Operation(description = "Add Instructor and Assign To Course")
     @PutMapping("/addAndAssignToCourse/{numCourse}")
-    public Instructor addAndAssignToInstructor(@RequestBody Instructor instructor, @PathVariable("numCourse")Long numCourse){
-        return  instructorServices.addInstructorAndAssignToCourse(instructor,numCourse);
+    public Instructor addAndAssignToInstructor(@RequestBody Instructor instructor, @PathVariable("numCourse") Long numCourse) {
+        logger.info("Received request to add instructor: {} and assign to course ID: {}", instructor, numCourse);
+        Instructor addedInstructor = instructorServices.addInstructorAndAssignToCourse(instructor, numCourse);
+        if (addedInstructor != null) {
+            logger.info("Instructor added and assigned to course successfully with ID: {}", addedInstructor.getNumInstructor());
+        } else {
+            logger.error("Failed to add instructor and assign to course ID: {}", numCourse);
+        }
+        return addedInstructor;
     }
+
     @Operation(description = "Retrieve all Instructors")
     @GetMapping("/all")
-    public List<Instructor> getAllInstructors(){
-        return instructorServices.retrieveAllInstructors();
+    public List<Instructor> getAllInstructors() {
+        logger.info("Received request to retrieve all instructors");
+        List<Instructor> instructors = instructorServices.retrieveAllInstructors();
+        logger.debug("Retrieved instructors: {}", instructors);
+        return instructors;
     }
 
     @Operation(description = "Update Instructor ")
     @PutMapping("/update")
-    public Instructor updateInstructor(@RequestBody Instructor Instructor){
-        return  instructorServices.updateInstructor(Instructor);
+    public Instructor updateInstructor(@RequestBody Instructor instructor) {
+        logger.info("Received request to update instructor: {}", instructor);
+        Instructor updatedInstructor = instructorServices.updateInstructor(instructor);
+        logger.info("Instructor updated successfully with ID: {}", updatedInstructor.getNumInstructor());
+        return updatedInstructor;
     }
 
     @Operation(description = "Retrieve Instructor by Id")
     @GetMapping("/get/{id-instructor}")
-    public Instructor getById(@PathVariable("id-instructor") Long numInstructor){
-        return instructorServices.retrieveInstructor(numInstructor);
+    public Instructor getById(@PathVariable("id-instructor") Long numInstructor) {
+        logger.info("Received request to retrieve instructor with ID: {}", numInstructor);
+        Instructor instructor = instructorServices.retrieveInstructor(numInstructor);
+        if (instructor == null) {
+            logger.warn("Instructor with ID: {} not found", numInstructor);
+        } else {
+            logger.info("Retrieved instructor: {}", instructor);
+        }
+        return instructor;
     }
-
 }
--- src/main/java/tn/esprit/spring/services/InstructorServicesImpl.java
+++ src/main/java/tn/esprit/spring/services/InstructorServicesImpl.java
@@ -1,6 +1,8 @@
 package tn.esprit.spring.services;
 
 import lombok.AllArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 import tn.esprit.spring.entities.Course;
 import tn.esprit.spring.entities.Instructor;
@@ -13,39 +15,62 @@ import java.util.Set;
 
 @AllArgsConstructor
 @Service
-public class InstructorServicesImpl implements IInstructorServices{
+public class InstructorServicesImpl implements IInstructorServices {
+
+    private static final Logger logger = LogManager.getLogger(InstructorServicesImpl.class);
 
     private IInstructorRepository instructorRepository;
     private ICourseRepository courseRepository;
 
     @Override
     public Instructor addInstructor(Instructor instructor) {
-        return instructorRepository.save(instructor);
+        logger.info("Adding new instructor: {}", instructor);
+        Instructor savedInstructor = instructorRepository.save(instructor);
+        logger.info("Instructor added successfully with ID: {}", savedInstructor.getNumInstructor());
+        return savedInstructor;
     }
 
     @Override
     public List<Instructor> retrieveAllInstructors() {
-        return instructorRepository.findAll();
+        logger.info("Retrieving all instructors");
+        List<Instructor> instructors = instructorRepository.findAll();
+        logger.debug("Instructors retrieved: {}", instructors);
+        return instructors;
     }
 
     @Override
     public Instructor updateInstructor(Instructor instructor) {
-        return instructorRepository.save(instructor);
+        logger.info("Updating instructor: {}", instructor);
+        Instructor updatedInstructor = instructorRepository.save(instructor);
+        logger.info("Instructor updated successfully with ID: {}", updatedInstructor.getNumInstructor());
+        return updatedInstructor;
     }
 
     @Override
     public Instructor retrieveInstructor(Long numInstructor) {
-        return instructorRepository.findById(numInstructor).orElse(null);
+        logger.info("Retrieving instructor with ID: {}", numInstructor);
+        Instructor instructor = instructorRepository.findById(numInstructor).orElse(null);
+        if (instructor == null) {
+            logger.warn("Instructor with ID: {} not found", numInstructor);
+        } else {
+            logger.info("Instructor retrieved: {}", instructor);
+        }
+        return instructor;
     }
 
     @Override
     public Instructor addInstructorAndAssignToCourse(Instructor instructor, Long numCourse) {
+        logger.info("Adding instructor: {} and assigning to course ID: {}", instructor, numCourse);
         Course course = courseRepository.findById(numCourse).orElse(null);
+        if (course == null) {
+            logger.error("Course with ID: {} not found. Instructor not added.", numCourse);
+            return null; // ou vous pouvez lancer une exception
+        }
         Set<Course> courseSet = new HashSet<>();
         courseSet.add(course);
         instructor.setCourses(courseSet);
-        return instructorRepository.save(instructor);
+        Instructor savedInstructor = instructorRepository.save(instructor);
+        logger.info("Instructor added and assigned to course successfully with ID: {}", savedInstructor.getNumInstructor());
+        return savedInstructor;
     }
-
-
 }
