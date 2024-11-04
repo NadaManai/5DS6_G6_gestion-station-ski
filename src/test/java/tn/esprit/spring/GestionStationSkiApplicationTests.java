17f47d398669760d4665380a1dd923e433c4a678
eyabenahmed <eya.benahmed@esprit.tn>
2024-11-04 12:39:28 +0100

    junit

--- src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
+++ src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
@@ -1,13 +1,30 @@
 package tn.esprit.spring;
-
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.spring.entities.Course;
+import tn.esprit.spring.services.ICourseServices;
+
+
+import java.util.List;
 
 @SpringBootTest
+@TestMethodOrder(OrderAnnotation.class)
+
+
 class GestionStationSkiApplicationTests {
 
+	@Autowired
+	ICourseServices cs;
 	@Test
-	void contextLoads() {
+	@Order(1)
+	void getAllCoursess() {
+		List<Course> listUsers = cs.retrieveAllCourses();
+		Assertions.assertEquals(0, listUsers.size());
+		}
 	}
 
-}
