b8283840ae592f2dc81eea4ccac690262d13f9eb
ameni0 <ameni.touihri@gmail.com>
2024-11-05 02:31:08 +0000

    Add log4j and SLF4 logs and debugging

--- src/main/java/tn/esprit/spring/controllers/RegistrationRestController.java
+++ src/main/java/tn/esprit/spring/controllers/RegistrationRestController.java
@@ -3,6 +3,8 @@ package tn.esprit.spring.controllers;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.spring.entities.Instructor;
 import tn.esprit.spring.entities.Registration;
@@ -17,36 +19,48 @@ import java.util.List;
 @RequestMapping("/registration")
 @RequiredArgsConstructor
 public class RegistrationRestController {
+
+    private static final Logger logger = LoggerFactory.getLogger(RegistrationRestController.class);
     private final IRegistrationServices registrationServices;
 
     @Operation(description = "Add Registration and Assign to Skier")
     @PutMapping("/addAndAssignToSkier/{numSkieur}")
     public Registration addAndAssignToSkier(@RequestBody Registration registration,
-                                                     @PathVariable("numSkieur") Long numSkieur)
-    {
-        return  registrationServices.addRegistrationAndAssignToSkier(registration,numSkieur);
-    }
-    @Operation(description = "Assign Registration to Course")
-    @PutMapping("/assignToCourse/{numRegis}/{numSkieur}")
-    public Registration assignToCourse( @PathVariable("numRegis") Long numRegistration,
-                                        @PathVariable("numSkieur") Long numCourse){
-        return registrationServices.assignRegistrationToCourse(numRegistration, numCourse);
+                                            @PathVariable("numSkieur") Long numSkieur) {
+        logger.info("Request to add and assign registration to skier with ID: {}", numSkieur);
+        Registration result = registrationServices.addRegistrationAndAssignToSkier(registration, numSkieur);
+        logger.info("Registration added and assigned to skier with ID: {}", numSkieur);
+        return result;
     }
 
+    @Operation(description = "Assign Registration to Course")
+    @PutMapping("/assignToCourse/{numRegis}/{numSkieur}")
+    public Registration assignToCourse(@PathVariable("numRegis") Long numRegistration,
+                                       @PathVariable("numSkieur") Long numCourse) {
+        logger.info("Request to assign registration with ID: {} to course with ID: {}", numRegistration, numCourse);
+        Registration result = registrationServices.assignRegistrationToCourse(numRegistration, numCourse);
+        logger.info("Registration with ID: {} assigned to course with ID: {}", numRegistration, numCourse);
+        return result;
+    }
 
     @Operation(description = "Add Registration and Assign to Skier and Course")
     @PutMapping("/addAndAssignToSkierAndCourse/{numSkieur}/{numCourse}")
     public Registration addAndAssignToSkierAndCourse(@RequestBody Registration registration,
                                                      @PathVariable("numSkieur") Long numSkieur,
-                                                     @PathVariable("numCourse") Long numCourse)
-    {
-        return  registrationServices.addRegistrationAndAssignToSkierAndCourse(registration,numSkieur,numCourse);
+                                                     @PathVariable("numCourse") Long numCourse) {
+        logger.info("Request to add registration and assign to skier with ID: {} and course with ID: {}", numSkieur, numCourse);
+        Registration result = registrationServices.addRegistrationAndAssignToSkierAndCourse(registration, numSkieur, numCourse);
+        logger.info("Registration added and assigned to skier with ID: {} and course with ID: {}", numSkieur, numCourse);
+        return result;
     }
 
     @Operation(description = "Numbers of the weeks when an instructor has given lessons in a given support")
     @GetMapping("/numWeeks/{numInstructor}/{support}")
-    public List<Integer> numWeeksCourseOfInstructorBySupport(@PathVariable("numInstructor")Long numInstructor,
-                                                                  @PathVariable("support") Support support) {
-        return registrationServices.numWeeksCourseOfInstructorBySupport(numInstructor,support);
+    public List<Integer> numWeeksCourseOfInstructorBySupport(@PathVariable("numInstructor") Long numInstructor,
+                                                             @PathVariable("support") Support support) {
+        logger.info("Request to retrieve number of weeks for instructor with ID: {} and support: {}", numInstructor, support);
+        List<Integer> result = registrationServices.numWeeksCourseOfInstructorBySupport(numInstructor, support);
+        logger.info("Retrieved number of weeks for instructor with ID: {} and support: {}", numInstructor, support);
+        return result;
     }
 }
--- src/main/java/tn/esprit/spring/services/RegistrationServicesImpl.java
+++ src/main/java/tn/esprit/spring/services/RegistrationServicesImpl.java
@@ -12,95 +12,116 @@ import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.List;
+
 @Slf4j
 @AllArgsConstructor
 @Service
-public class RegistrationServicesImpl implements  IRegistrationServices{
+public class RegistrationServicesImpl implements IRegistrationServices {
 
-    private IRegistrationRepository registrationRepository;
-    private ISkierRepository skierRepository;
-    private ICourseRepository courseRepository;
-
+    private final IRegistrationRepository registrationRepository;
+    private final ISkierRepository skierRepository;
+    private final ICourseRepository courseRepository;
 
     @Override
     public Registration addRegistrationAndAssignToSkier(Registration registration, Long numSkier) {
+        log.info("Adding registration and assigning to skier with ID: {}", numSkier);
         Skier skier = skierRepository.findById(numSkier).orElse(null);
+        if (skier == null) {
+            log.warn("Skier with ID: {} not found.", numSkier);
+            return null;
+        }
         registration.setSkier(skier);
-        return registrationRepository.save(registration);
+        Registration savedRegistration = registrationRepository.save(registration);
+        log.info("Registration added and assigned to skier with ID: {}", numSkier);
+        return savedRegistration;
     }
 
     @Override
     public Registration assignRegistrationToCourse(Long numRegistration, Long numCourse) {
+        log.info("Assigning registration with ID: {} to course with ID: {}", numRegistration, numCourse);
         Registration registration = registrationRepository.findById(numRegistration).orElse(null);
         Course course = courseRepository.findById(numCourse).orElse(null);
+        if (registration == null || course == null) {
+            log.warn("Either registration ID: {} or course ID: {} was not found.", numRegistration, numCourse);
+            return null;
+        }
         registration.setCourse(course);
-        return registrationRepository.save(registration);
+        Registration savedRegistration = registrationRepository.save(registration);
+        log.info("Registration with ID: {} assigned to course with ID: {}", numRegistration, numCourse);
+        return savedRegistration;
     }
 
     @Transactional
     @Override
     public Registration addRegistrationAndAssignToSkierAndCourse(Registration registration, Long numSkieur, Long numCours) {
+        log.info("Adding registration and assigning to skier with ID: {} and course with ID: {}", numSkieur, numCours);
         Skier skier = skierRepository.findById(numSkieur).orElse(null);
         Course course = courseRepository.findById(numCours).orElse(null);
 
         if (skier == null || course == null) {
+            log.warn("Either skier ID: {} or course ID: {} was not found.", numSkieur, numCours);
             return null;
         }
 
-        if(registrationRepository.countDistinctByNumWeekAndSkier_NumSkierAndCourse_NumCourse(registration.getNumWeek(), skier.getNumSkier(), course.getNumCourse()) >=1){
-            log.info("Sorry, you're already register to this course of the week :" + registration.getNumWeek());
+        if (registrationRepository.countDistinctByNumWeekAndSkier_NumSkierAndCourse_NumCourse(registration.getNumWeek(), skier.getNumSkier(), course.getNumCourse()) >= 1) {
+            log.warn("Skier with ID: {} is already registered for the course in week: {}", skier.getNumSkier(), registration.getNumWeek());
             return null;
         }
 
         int ageSkieur = Period.between(skier.getDateOfBirth(), LocalDate.now()).getYears();
-        log.info("Age " + ageSkieur);
+        log.info("Skier's age: {}", ageSkieur);
 
         switch (course.getTypeCourse()) {
             case INDIVIDUAL:
-                log.info("add without tests");
+                log.info("Assigning individual course without further checks.");
                 return assignRegistration(registration, skier, course);
 
             case COLLECTIVE_CHILDREN:
                 if (ageSkieur < 16) {
-                    log.info("Ok CHILD !");
+                    log.info("Assigning to Collective Children Course.");
                     if (registrationRepository.countByCourseAndNumWeek(course, registration.getNumWeek()) < 6) {
-                        log.info("Course successfully added !");
+                        log.info("Course slot available. Registration successful.");
                         return assignRegistration(registration, skier, course);
                     } else {
-                        log.info("Full Course ! Please choose another week to register !");
+                        log.warn("Course is full. Registration unsuccessful.");
                         return null;
                     }
-                }
-                else{
-                    log.info("Sorry, your age doesn't allow you to register for this course ! \n Try to Register to a Collective Adult Course...");
+                } else {
+                    log.warn("Skier's age does not qualify for Collective Children Course.");
                 }
                 break;
 
             default:
                 if (ageSkieur >= 16) {
-                    log.info("Ok ADULT !");
+                    log.info("Assigning to Collective Adult Course.");
                     if (registrationRepository.countByCourseAndNumWeek(course, registration.getNumWeek()) < 6) {
-                        log.info("Course successfully added !");
+                        log.info("Course slot available. Registration successful.");
                         return assignRegistration(registration, skier, course);
                     } else {
-                        log.info("Full Course ! Please choose another week to register !");
+                        log.warn("Course is full. Registration unsuccessful.");
                         return null;
                     }
+                } else {
+                    log.warn("Skier's age does not qualify for Collective Adult Course.");
                 }
-                log.info("Sorry, your age doesn't allow you to register for this course ! \n Try to Register to a Collective Child Course...");
         }
         return registration;
-
     }
-    private Registration assignRegistration (Registration registration, Skier skier, Course course){
+
+    private Registration assignRegistration(Registration registration, Skier skier, Course course) {
+        log.info("Assigning skier ID: {} to course ID: {}", skier.getNumSkier(), course.getNumCourse());
         registration.setSkier(skier);
         registration.setCourse(course);
-        return registrationRepository.save(registration);
+        Registration savedRegistration = registrationRepository.save(registration);
+        log.info("Registration completed for skier ID: {} to course ID: {}", skier.getNumSkier(), course.getNumCourse());
+        return savedRegistration;
     }
 
     @Override
     public List<Integer> numWeeksCourseOfInstructorBySupport(Long numInstructor, Support support) {
-        return registrationRepository.numWeeksCourseOfInstructorBySupport(numInstructor, support);
+        log.info("Retrieving number of weeks for instructor ID: {} with support: {}", numInstructor, support);
+        List<Integer> weeks = registrationRepository.numWeeksCourseOfInstructorBySupport(numInstructor, support);
+        log.info("Number of weeks retrieved: {}", weeks);
+        return weeks;
     }
-
 }
